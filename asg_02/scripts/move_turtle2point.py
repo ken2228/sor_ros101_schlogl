499aa04ff7c004d436c1824ba3b6fbf60ff3b8f5
ken2228 <danny_sleugel@hotmail.com>
2023-05-22 22:05:47 +0200

    modified:   asg_02/scripts/move_turtle2point.py

--- asg_02/scripts/move_turtle2point.py
+++ asg_02/scripts/move_turtle2point.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
 
 import rospy
+import math
+import time
 
 from geometry_msgs.msg import Point, Twist
 from turtlesim.msg import Pose
 
-import math
-
 target_received = False
 pose_received = False
 
-target = Point()
+targetPoint= Point()
 current_pose = Pose()
 
 def targetCallback(data):
-	global target
+	global targetPoint
 	global target_received
-	target = data
+	targetPoint = data
 	target_received = True
 		
 def poseCallback(data):
@@ -25,11 +25,39 @@ def poseCallback(data):
 	current_pose = data
 	pose_received = True
 
+
+def rotate2target():
+	twistMsg = Twist()
+	multiplier = 1
+	rospy.loginfo("rotating to: %f !", current_pose.theta)
+	targetHeading = math.atan2(targetPoint.x - current_pose.x, targetPoint.y - current_pose.y)
+ 	
+ 	
+	while float(current_pose.theta) != float(targetHeading):
+		rospy.loginfo("currentheading = %f, targetheading = %f", current_pose.theta, targetHeading)
+		twistMsg.linear.x = 0.0
+		
+		if current_pose.theta > targetHeading:
+			multiplier = -1
+		
+		twistMsg.angular.z = abs(current_pose.theta - targetHeading)* multiplier # -1*math.pi/2.0
+		pub.publish(twistMsg)
+		rospy.sleep(0.1)
+		if int(current_pose.theta * 100) == int(targetHeading * 100):
+			break
+			
+	twistMsg.angular.z = 0.0
+	pub.publish(twistMsg)
+	rospy.loginfo("End of turning reached!")
+ 	
+ 	
+ 	
 def move2target():
 	#make calculations to move to the target
 	if target_received and pose_received:
-		rospy.loginfo("target and pose received, calculating!")
+		rospy.loginfo("targetPoint and pose received, calculating!")
 		rospy.loginfo("Current Pose: %f, %f | theta %f", current_pose.x, current_pose.y, current_pose.theta)
+		rotate2target()
 		
 
 
